7b924f7e86812ce3bf1daa934aaf4ee938dd68bc
Martijn Kooijman <martijn@fresh-dev.nl>
2024-02-12 12:55:14 +0100

    Added Hamburger menu

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,30 +1,31 @@
 // Vervang 'jouw_bestand.json' door het pad naar jouw JSON-bestand
 
 function fetchData() {
-  const bestandPad = "https://martino538.github.io/web-app-from-scratch-2324/assets/dataset/dataset-Martijn.json";
+  const bestandPad =
+    "https://martino538.github.io/web-app-from-scratch-2324/assets/dataset/dataset-Martijn.json";
 
   fetch(bestandPad)
-  .then((response) => {
-    // Controleer of het ophalen succesvol was (status code 200-299)
-    if (!response.ok) {
-      throw new Error(
-        `Fout bij het ophalen van het bestand: ${response.status}`
+    .then((response) => {
+      // Controleer of het ophalen succesvol was (status code 200-299)
+      if (!response.ok) {
+        throw new Error(
+          `Fout bij het ophalen van het bestand: ${response.status}`
+        );
+      }
+      // Converteer de response naar JSON
+      return response.json();
+    })
+    .then((data) => {
+      // Verwerk de JSON-data hier
+      setInterval(() => greetUser(data), 5000);
+      getAuthorInfo(data);
+    })
+    .catch((error) => {
+      console.error(
+        "Er is een fout opgetreden bij het ophalen van het bestand:",
+        error
       );
-    }
-    // Converteer de response naar JSON
-    return response.json();
-  })
-  .then((data) => {
-    // Verwerk de JSON-data hier
-    setInterval(() => greetUser(data), 5000);
-    getAuthorInfo(data);
-  })
-  .catch((error) => {
-    console.error(
-      "Er is een fout opgetreden bij het ophalen van het bestand:",
-      error
-    );
-  });
+    });
 }
 
 // Random greeting word generator
@@ -35,9 +36,9 @@ function greetUser(data) {
 
   const randomIndex = getRandomInt();
   const randomGreeting = data.greetings[randomIndex];
-  
+
   const greetUser = document.getElementById("greeting-user");
-  greetUser.textContent = '';
+  greetUser.textContent = "";
   greetUser.textContent = randomGreeting;
 }
 
@@ -51,7 +52,7 @@ function getAuthorInfo(data) {
   const authorImg = document.getElementById("pageheader-img");
   const heroesList = document.getElementById("heroes-list");
   const villainList = document.getElementById("villain-list");
-  
+
   authorName.textContent = data.name;
   authorAge.textContent = data.age;
   authorCity.textContent = data.city;
@@ -59,14 +60,13 @@ function getAuthorInfo(data) {
   authorJob.textContent = data.job;
   authorImg.src = data.avatar.url;
 
-
-  data.topHeroes.forEach(hero => {
+  data.topHeroes.forEach((hero) => {
     const li = document.createElement("li");
     li.textContent = hero;
     heroesList.appendChild(li);
   });
 
-  data.topVillains.forEach(villain => {
+  data.topVillains.forEach((villain) => {
     const li = document.createElement("li");
     li.textContent = villain;
     villainList.appendChild(li);
@@ -74,14 +74,24 @@ function getAuthorInfo(data) {
 }
 
 function moveImageText() {
-  const selectedItem = document.querySelector('body');
-  selectedItem.classList.add('animating-header');
+  const selectedItem = document.querySelector("body");
+  selectedItem.classList.add("animating-header");
 }
 
+function hamburgerMenu() {
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const navMenu = document.getElementById("nav-menu");
 
-  const animateBtn = document.getElementById('animate-btn');
-  animateBtn.addEventListener("click", moveImageText);
+  console.log(hamburgerMenu);
 
+  hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+}
 
+const animateBtn = document.getElementById("animate-btn");
+animateBtn.addEventListener("click", moveImageText);
 
 fetchData();
+hamburgerMenu();
